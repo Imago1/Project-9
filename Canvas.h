02fb471f3f3fd966d6c9c1ee7175ad2206783773
Imago1 <59735441+Imago1@users.noreply.github.com>
2022-02-08 20:56:17 +0100

    Update Canvas.h

--- Canvas.h
+++ Canvas.h
@@ -5,19 +5,19 @@
 using namespace std;
 
 
-const int DIGIT_LENGTH = 10; //дължината на една цифра от таймера в char-ове (от условието)
-const int DIGIT_COUNT = 6; // колко цифри има на таймера (от условието)
-const int LENGTH = DIGIT_LENGTH * DIGIT_COUNT; // дължината на целия таймер в char-ове(от условието)
-const int WIDTH = 11;//ширината на таймера(от условието)
+const int DIGIT_LENGTH = 10; 
+const int DIGIT_COUNT = 6; 
+const int LENGTH = DIGIT_LENGTH * DIGIT_COUNT; 
+const int WIDTH = 11;
 
 class Canvas {
 public:
-	char arr[WIDTH][LENGTH];// двумерен масив, в него рисуваме отделните цифри от таймера
-	int time = 0; // колко време остава до 00:00:00 в секунди
-	int hours = 0;//таймерът ще се показва като hours:minutes:seconds
+	char arr[WIDTH][LENGTH];
+	int time = 0; 
+	int hours = 0;
 	int minutes = 0;
 	int seconds = 0;
-	int colours[DIGIT_COUNT];//ползваме го после заради условието цифрите да са в произволен цвят
+	int colours[DIGIT_COUNT];
 
 
 	Canvas(int secs);
